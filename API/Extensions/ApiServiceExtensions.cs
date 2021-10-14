7e5a0d1b655bd7802f6f4ad0d1d31015720c69ba
Olashupo Ajala <oajala2@students.towson.edu>
2021-10-14 15:51:15 -0400

    End of section 4

---
+++ API/Extensions/ApiServiceExtensions.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace API.Extensions
+{
+    public static class ApiServiceExtensions
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
+
+            return services;
+        }
+    }
+}
